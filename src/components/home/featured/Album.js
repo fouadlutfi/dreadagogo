40541c36044b5b14539908572f4a4cf28b385ba2
Fouad Lutfi <thefouad@gmail.com>
2020-02-29 20:23:43 -0600

    bruh

--- src/components/home/featured/Album.js
+++ src/components/home/featured/Album.js
@@ -6,7 +6,7 @@ const Album = () => (
         <h3 className="text-center mb-md-3 mb-xl-5">DEMO</h3>,
         <Jumbotron className="album mb-2 mx-auto" />,
         <div className="text-center mb-5">
-            <a className="d-inline-block mx-auto album-link" href="">Order Now</a>
+            <a className="d-inline-block mx-auto album-link" href="kdmc">Order Now</a>
         </div>                  
     ]
 );
